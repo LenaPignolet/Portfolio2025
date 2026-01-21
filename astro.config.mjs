9facaf4636a64b084268feda35b0f7684ac6ffbb
Léna Pignolet <lena.pignolet@dnd.fr>
2026-01-21 23:12:30 +0100

    Add vite plugin svgr and lucide icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
+import svgr from "vite-plugin-svgr";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [svgr()],
+  },
   integrations: [vue()]
 });
